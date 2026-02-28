1f317f8c0e553a9abd870bd3795a74b128eb2f6e
manikumar <manikumar934@gmail.com>
2026-02-28 07:16:38 +0530

    r

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -4,5 +4,8 @@ terraform {
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock"
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_credentials_validation = true
   }
 }
