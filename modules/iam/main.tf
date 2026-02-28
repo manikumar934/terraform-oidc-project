83d650abf86081ce2988377c37b59964b0cf2e34
manikumar <manikumar934@gmail.com>
2026-02-28 07:01:36 +0530

    3commmit

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -2,3 +2,44 @@ resource "aws_iam_role" "this" {
   name               = var.role_name
   assume_role_policy = var.assume_role_policy
 }
+
+resource "aws_iam_role_policy" "terraform_backend" {
+  name = "terraform-backend-access"
+  role = aws_iam_role.this.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::mani-terraform-state",
+          "arn:aws:s3:::mani-terraform-state/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:DeleteItem"
+        ]
+        Resource = "arn:aws:dynamodb:us-east-1:427814062472:table/terraform-lock"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:*",
+          "s3:*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
