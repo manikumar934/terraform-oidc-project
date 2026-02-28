d8cf54b917d1fd4731fd3c8bca8efa8218e92735
manikumar <manikumar934@gmail.com>
2026-02-28 06:11:39 +0530

    changes-made

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "mani-terraform-state"
+    bucket         = "mani-tfstate-2026-01"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock"
