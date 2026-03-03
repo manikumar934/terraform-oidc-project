b161b6da7aa0bcb8bd956d00ef252be554357e54
manikumar <manikumar934@gmail.com>
2026-03-03 13:21:37 +0530

    ec2

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,3 +1,3 @@
 ami           = "ami-0f3caa1cf4417e51b"
-instance_type = "t3.micro"
+instance_type = "t2.micro"
 bucket_name   = "mani-dev-bucket-12345"
--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,4 +1,15 @@
-variable "ami" {}
-variable "instance_type" {}
-variable "name" {}
-variable "environment" {}
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"   # Free Tier eligible
+}
+
+variable "name" {
+  description = "Name tag for EC2"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment tag (dev/prod)"
+  type        = string
+}
