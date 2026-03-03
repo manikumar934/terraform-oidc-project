e422121e40de68bf977165ae6b4847612580bab8
manikumar <manikumar934@gmail.com>
2026-03-03 13:02:51 +0530

    remove-dynamodb

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,11 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "mani-terraform-state01"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    skip_credentials_validation = true
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
